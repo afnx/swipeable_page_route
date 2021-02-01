f902e87e04617d3e7fabe940952a62da2db5c380
Ali Fuat Numanoglu <12843244+afnx@users.noreply.github.com>
2021-02-01 22:27:00 +0300

    Update app_bar.dart

--- lib/src/app_bar/app_bar.dart
+++ lib/src/app_bar/app_bar.dart
@@ -111,30 +111,33 @@ class MorphingAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Hero(
-      tag: heroTag,
-      flightShuttleBuilder: _flightShuttleBuilder,
-      transitionOnUserGestures: true,
-      child: AppBar(
-        leading: leading,
-        automaticallyImplyLeading: automaticallyImplyLeading,
-        title: title,
-        actions: actions,
-        flexibleSpace: flexibleSpace,
-        bottom: bottom,
-        elevation: elevation,
-        shape: shape,
-        backgroundColor: backgroundColor,
-        brightness: brightness,
-        iconTheme: iconTheme,
-        actionsIconTheme: actionsIconTheme,
-        textTheme: textTheme,
-        primary: primary,
-        centerTitle: centerTitle,
-        titleSpacing: titleSpacing,
-        toolbarOpacity: toolbarOpacity,
-        bottomOpacity: bottomOpacity,
-        leadingWidth: leadingWidth,
+    return Material(
+      color: Colors.transparent,
+      child: Hero(
+        tag: heroTag,
+        flightShuttleBuilder: _flightShuttleBuilder,
+        transitionOnUserGestures: true,
+        child: AppBar(
+          leading: leading,
+          automaticallyImplyLeading: automaticallyImplyLeading,
+          title: title,
+          actions: actions,
+          flexibleSpace: flexibleSpace,
+          bottom: bottom,
+          elevation: elevation,
+          shape: shape,
+          backgroundColor: backgroundColor,
+          brightness: brightness,
+          iconTheme: iconTheme,
+          actionsIconTheme: actionsIconTheme,
+          textTheme: textTheme,
+          primary: primary,
+          centerTitle: centerTitle,
+          titleSpacing: titleSpacing,
+          toolbarOpacity: toolbarOpacity,
+          bottomOpacity: bottomOpacity,
+          leadingWidth: leadingWidth,
+        ),
       ),
     );
   }
@@ -187,26 +190,31 @@ class _AnimatedAppBar extends AnimatedWidget {
   Widget build(BuildContext context) {
     final state = MorphingState(parent: parent, child: child, t: t);
 
-    return Scaffold(
-      backgroundColor: Colors.transparent,
-      appBar: AppBar(
-        leading: AnimatedLeading(state),
-        // We manually determine the leadings to be able to animate between them.
-        automaticallyImplyLeading: false,
-        title: AnimatedTitle(state),
-        actions: [AnimatedActions(state)],
-        bottom: AnimatedBottom(state),
-        elevation:
-            lerpDouble(_resolveElevation(parent), _resolveElevation(child), t),
-        shape: ShapeBorder.lerp(parent.appBar.shape, child.appBar.shape, t),
-        brightness: state.brightness,
-        backgroundColor: state.backgroundColor,
-        leadingWidth: state.parent.appBar.leadingWidth,
-        // iconTheme & actionsIconTheme are applied in AnimatedLeading &
-        // AnimatedActions directly to differentiate between parent & child.
-        // Value is the same for parent and child, so it doesn't matter which one
-        // we use.
-        primary: parent.appBar.primary,
+    return IgnorePointer(
+      ignoring: true,
+      child: Material(
+        color: Colors.transparent,
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            leading: AnimatedLeading(state),
+            // We manually determine the leadings to be able to animate between them.
+            automaticallyImplyLeading: false,
+            title: AnimatedTitle(state),
+            actions: [AnimatedActions(state)],
+            bottom: AnimatedBottom(state),
+            elevation: lerpDouble(_resolveElevation(parent), _resolveElevation(child), t),
+            shape: ShapeBorder.lerp(parent.appBar.shape, child.appBar.shape, t),
+            brightness: state.brightness,
+            backgroundColor: state.backgroundColor,
+            leadingWidth: state.parent.appBar.leadingWidth,
+            // iconTheme & actionsIconTheme are applied in AnimatedLeading &
+            // AnimatedActions directly to differentiate between parent & child.
+            // Value is the same for parent and child, so it doesn't matter which one
+            // we use.
+            primary: parent.appBar.primary,
+          ),
+        ),
       ),
     );
   }
